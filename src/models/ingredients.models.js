047799182164b3f40e7edf5e7e7b10dc3930e6a4
SheykoWk <sheyko.wk@gmail.com>
2022-10-27 19:38:07 -0500

    add ingredients model

---
+++ src/models/ingredients.models.js
@@ -0,0 +1,38 @@
+const { DataTypes } = require('sequelize')
+
+const db = require('../utils/database')
+const Types = require('./types.models')
+
+const Ingredients = db.define('ingredients', {
+    id: {
+        type: DataTypes.UUID,
+        primaryKey: true,
+        allowNull: false
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull:false,
+        unique: true
+    }, //? holaGrupoComoEstan -> Javascript
+       //? hola_gripo_como_estan -> SQL
+    typeId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        field: 'type_id',
+        references: {
+            key: 'id',
+            model: Types
+        }
+    },
+    urlImg : {
+        type: DataTypes.STRING,
+        field: 'url_img',
+        validate: {
+            isUrl: true
+        }
+    }
+}, {
+    timestamps: false
+})
+
+module.exports = Ingredients
