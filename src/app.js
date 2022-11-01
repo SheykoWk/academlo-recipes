059b204a01225300c033fe3e8067dd51232a45ed
SheykoWk <sheyko.wk@gmail.com>
2022-10-31 21:31:18 -0600

    add recipe to main app

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ const {port} = require('./config');
 const userRouter = require('./users/users.router')
 const authRouter = require('./auth/auth.router')
 const categoryRouter = require('./categories/categories.router')
-
+const recipeRouter = require('./recipes/recipes.router')
 
 const initModels = require('./models/initModels')
 
@@ -49,6 +49,7 @@ app.get('/',(req, res) => {
 app.use('/api/v1/users', userRouter)
 app.use('/api/v1/auth', authRouter)
 app.use('/api/v1/categories', categoryRouter)
+app.use('/api/v1/recipes', recipeRouter)
 
 app.listen(port, () => {
     console.log(`Server started at port ${port}`)
