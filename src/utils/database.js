663c2931ede8af5e7123c55c2e9679c39cf1cae8
SheykoWk <sheyko.wk@gmail.com>
2022-11-03 18:11:16 -0600

    add ssl for deploy

--- src/utils/database.js
+++ src/utils/database.js
@@ -6,7 +6,16 @@ const db = new Sequelize({
     host: config.db.host, //? Variable de entorno del host
     username: config.db.username, //? Variable de entorno del usuario
     password: config.db.password, //? Variable de entorno de la contraseña
-    database: config.db.dbName //? Variable de entorno de la base de datos
+    database: config.db.dbName, //? Variable de entorno de la base de datos
+    dialectOptions:
+    process.env.NODE_ENV === 'production'
+      ? {
+          ssl: {
+            require: true,
+            rejectUnauthorized: false,
+          },
+        }
+      : {},
 })
 
 module.exports = db
